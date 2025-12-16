7b2628a5c19f3c9e8af1a90a0fc41e9617b022ea
Arda Günal <102471766+Jumpingquasar@users.noreply.github.com>
2025-12-16 16:44:06 +0300

    add basic torque simulation

--- app.js
+++ app.js
@@ -1,8 +1,47 @@
 'use strict';
 
+const seesawPlankElement = document.querySelector('#seesaw-plank');
+const plankCenterX = seesawPlankElement.offsetWidth / 2;
+var currentWeight = 10;
+var totalTorque = 0;
+var leftWeight = 0;
+var rightWeight = 0;
+const objects = [];
 
+// Sets the angle of the seesaw plank
+function setSeesawAngle() {
+    const angle = Math.max(
+        -30,
+        Math.min(30, totalTorque)
+    );
+    seesawPlankElement.style.transform = `rotate(${angle}deg)`;
+}
 
-(function main() {
-    const message = 'Hello, world!';
-    console.log(message);
-})();
+// Appends new weight to the seesaw plank
+function addWeightToSeesaw(clickX) {
+
+    // Generate a new object and position it
+    const obj = document.createElement('div');
+    obj.className = 'object';
+    obj.style.left = `${clickX}px`;  
+    seesawPlankElement.appendChild(obj);
+
+    // Calculate the total torque of the system
+    totalTorque += currentWeight * (clickX) / 100;
+
+    // Set the new angle of the seesaw
+    setSeesawAngle();
+
+    // Randomize the next weight
+    currentWeight = Math.floor(Math.random() * 10) + 1;
+    document.querySelector('#next-weight-value').textContent = `${currentWeight} kg`;
+    console.log(`Added weight: ${currentWeight} kg at position: ${clickX}px`);
+    console.log(totalTorque);
+}
+
+// Listens for clicks on the seesaw plank
+seesawPlankElement.addEventListener('click', (event) => {
+    const rect = seesawPlankElement.getBoundingClientRect()
+    const x = event.clientX - rect.left
+    addWeightToSeesaw(x - plankCenterX);
+});
